dd6bd16d38c209a2e197777d55f9190e8a3f1744
Nicolas Leport <nicolas.leport@nkcreation.com>
2022-09-28 23:04:58 +0200

    add purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import vue from "@astrojs/vue";
 import compress from "astro-compress";
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
-
 import * as dotenv from 'dotenv';
-dotenv.config()
+import purgecss from "astro-purgecss";
+dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), compress(), sitemap()],
+  integrations: [vue(), compress(), sitemap(), purgecss()],
   vite: {
     ssr: {
       external: ["svgo"]
