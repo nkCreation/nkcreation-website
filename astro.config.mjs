ee443671d447a80059f91929112063f01df0e6f9
Nicolas Leport <nicolas.leport@nkcreation.com>
2022-09-28 22:52:27 +0200

    add sitemap and compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
+import vue from "@astrojs/vue";
+import compress from "astro-compress";
 import { defineConfig } from 'astro/config';
 
-import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap";
+
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), compress(), sitemap()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
+  site: `${process.env.HOSTNAME || process.env.DEPLOY_PRIME_URL || process.env.URL}`
 });
